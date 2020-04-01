b452cda06b571bc7335d0e6db4465852450d09c3
Andres Giraldo <andresganc@gmail.com>
2020-03-31 19:28:27 -0500

    New: Proyectos curso-nc. Mod: curso-udemy

---
+++ curso-nc/src/views/basico/hola-mundo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function HolaMundo () {
+
+    return (
+        <h2> !! Hola Mundo !! </h2>
+    );
+}
+
+export default HolaMundo;
