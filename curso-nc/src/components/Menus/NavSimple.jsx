bfa0ea710b3739273501567243ff93d67c18f393
Andres Giraldo <andresganc@gmail.com>
2020-06-05 12:01:58 -0500

    New: Nav MaterialUI

---
+++ curso-nc/src/components/Menus/NavSimple.jsx
@@ -0,0 +1,50 @@
+
+import React from 'react';
+
+// Components
+
+
+
+// Styles
+import StylesNav from '../../assets/styles/menus/nav';
+
+// Assets
+import ImagenInicio from '../../assets/img/home.svg';
+import ImagenMenu from '../../assets/img/menu.svg';
+
+
+class Menu extends React.Component {
+
+        render() {
+
+            return(
+                <div>
+                    
+                    <nav style={StylesNav.nav} >
+                        
+                        <div style={StylesNav.div_nav} >
+                            <ul>
+                                <img src={ImagenInicio} alt="" width="20px" />
+                                <li style={StylesNav.li} > <a style={StylesNav.a} href=""> Inicio </a> </li>
+                                <img src={ImagenMenu} alt="" width="20px" />
+                                <li style={StylesNav.li} > <a style={StylesNav.a} href=""> Menu 1 </a> </li>
+                                <img src={ImagenMenu} alt="" width="20px" />
+                                <li style={StylesNav.li} > <a style={StylesNav.a} href=""> Menu 2 </a> </li>
+                                <img src={ImagenMenu} alt="" width="20px" />
+                                <li style={StylesNav.li} > <a style={StylesNav.a} href=""> Menu 3 </a> </li>
+                                <img src={ImagenMenu} alt="" width="20px" />
+                                <li style={StylesNav.li} > <a style={StylesNav.a} href=""> Menu 4 </a> </li>
+                            </ul>
+                        </div>
+
+                    </nav> 
+
+                </div>
+            );
+
+        }
+
+}
+
+
+export default Menu;
--- curso-nc/src/index.js
+++ curso-nc/src/index.js
@@ -3,10 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Componentes
-import Menu from './components/menus/Nav';
-import MenuBuscar from './components/menus/Nav-Buscar';
-import ComponenteConProps from './components/curso/Props'
-import EstadoAlarma from './components/curso/State'
+
 
 
 
@@ -21,10 +18,7 @@ import ComponentesConClases from './components/ComponentesConClases';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Menu />
-    <MenuBuscar />
-    <ComponenteConProps texto="SUMAR" />
-    <EstadoAlarma />
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
