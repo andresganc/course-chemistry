1e9f459c66a6b690a2741f168169109c6ed616e4
Andres Giraldo <andresganc@gmail.com>
2020-10-28 01:11:11 -0500

    New: foto. Mod: Estructura header y estilos header

--- proyectos/landing-page-andres-giraldo/src/assets/styles/landing-page.js
+++ proyectos/landing-page-andres-giraldo/src/assets/styles/landing-page.js
@@ -1,13 +1,66 @@
 
 
+const ladingPageStyles = {
 
-const headerStyles = {
-    header: {
-        backgroundColor: '#202124'  
-    }
+    
+    // GRID CONTAINERS
+    gridContainerRoot: {
+        backgroundColor: '#000000'
+    },
+    gridContainerHeader: {
+
+    },
+    gridContainerSection: {
+
+    },
+    gridContainerFooter: {
+
+    },
+    
+    // GRID ITEM
+    gridItemNav: {
+        '@global h4': {
+            color: '#fff',
+        }
+    },
+
+    gridItemPerfil: {
+        padding: '20px',
+        alignContent: 'center',
+        textAlign: 'center',
+        '@global h2' : {
+            color: '#fff',
+            margin: '5px',
+            fontSize: '50px',
+            paddingTop: '60px'
+        },
+        '@global h3': {
+            color: '#fff',
+            fontSize: '40px'
+        },
+        '@global h4' : {
+            color: '#fff',
+            fontSize: '30px'
+        },
+        '@global h5' : {
+            color: '#fff',
+            fontSize: '20px'
+        }
+        
+    },
+
+    gridItemFotoPerfil: {
+
+    },
+
+
+    appBar: {
+        
+    },
+
     
 }
 
 
 
-export default headerStyles;
+export default ladingPageStyles;
--- proyectos/landing-page-andres-giraldo/src/landing-page.jsx
+++ proyectos/landing-page-andres-giraldo/src/landing-page.jsx
@@ -6,45 +6,55 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
 // Styles
-import headerStyles from './assets/styles/landing-page';
+import ladingPageStyles from './assets/styles/landing-page';
 
-const useStyles = makeStyles(headerStyles);
+// Images
+import AndresGiraldo from './assets/img/people-man-dark.jpg'
 
-// Styles in line 
-
+const useStyles = makeStyles(ladingPageStyles);
 
 
 function LandingPage() {
-  const classes = useStyles(useStyles);
+  const classes = useStyles();
   return (
 
     <Fragment>
-      <Grid className={classes.header} container spacing={1}>
-        <Grid container item xs={12} spacing={3}>
-          <h2>Grid Container 1</h2>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" className={classes.title}>
-                Dev - Andres Giraldo
-          </Typography>
-              <Button color="inherit">Login</Button>
-            </Toolbar>
-          </AppBar>
+      <Grid container spacing={2} className={classes.gridContainerRoot}>
+        
+        <Grid container item xs={12} spacing={3} className={classes.gridContainerHeader}>
+          <Grid item xs={12} spacing={3} className={classes.gridItemNav}>
+            <AppBar position="static" color='transparent'>
+              <Toolbar variant="dense">
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                  <MenuIcon />
+                </IconButton>
+                <h4>Andres Giraldo</h4>
+              </Toolbar>
+            </AppBar>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={6} spacing={3} className={classes.gridItemPerfil}>
+            <h2>Andres Giraldo</h2>
+            <h4>Desarrollador</h4>
+            <h5>JavaScript</h5>
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} spacing={3} className={classes.gridItemFotoPerfil}>
+              <img src={AndresGiraldo} alt=""/>
+          </Grid>
         </Grid>
+
         <Grid container item xs={12} spacing={3}>
           <h2>Grid Container 2</h2>
         </Grid>
+
         <Grid container item xs={12} spacing={3}>
           <h2>Grid Container 3</h2>
         </Grid>
+
       </Grid>
 
     </Fragment>
