cd1765e6f99cd2aa69c5b9dbcc42e70a7fb46dde
Andres Giraldo <andresganc@gmail.com>
2020-12-10 18:15:37 -0500

    Del: Cursos antiguos

---
+++ curso-nc/src/views/1-basic/styles/styles-module-css/StylesModule.tsx
@@ -0,0 +1,12 @@
+
+import React, { Fragment } from 'react'
+
+function StylesModule() {
+    return (
+        <Fragment>
+            <h3> Styles Module </h3>
+        </Fragment>
+    )
+}
+
+export default StylesModule
