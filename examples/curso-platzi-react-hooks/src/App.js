7a9a70d3fd8bb14de63dcea51c18ceae3c54d40b
Andres Giraldo <andresganc@gmail.com>
2021-03-16 10:21:07 -0500

    New: useState, useEffect

---
+++ examples/curso-platzi-react-hooks/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Characters from './components/Characters';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Characters />
+      <h1>Hola Mundo</h1>
+    </div>
+  );
+}
+
+export default App;
