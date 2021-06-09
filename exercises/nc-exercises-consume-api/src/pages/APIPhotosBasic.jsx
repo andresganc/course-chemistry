2796672c496efc761dda63f17a871bdcd3600702
Andres Giraldo <andresganc@gmail.com>
2021-06-09 08:02:08 -0500

    Mod: APIPhotoBasic

---
+++ exercises/nc-exercises-consume-api/src/pages/APIPhotosBasic.jsx
@@ -0,0 +1,12 @@
+
+import React, { Fragment } from 'react'
+
+const APIPhotosBasic = () => {
+    return (
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+export default APIPhotosBasic
